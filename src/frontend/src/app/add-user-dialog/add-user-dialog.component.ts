b81e7587b2f43281b18024d93fafb8422f525667
root <vipzshady@gmail.com>
2020-02-18 10:02:54 +0530

    Add user adding form

---
+++ src/frontend/src/app/add-user-dialog/add-user-dialog.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+export interface DialogData {
+  email: string;
+  name: string;
+  mobile_no: string;
+  role: string;
+}
+
+@Component({
+  selector: 'app-add-user-dialog',
+  templateUrl: './add-user-dialog.component.html',
+  styleUrls: ['./add-user-dialog.component.css']
+})
+export class AddUserDialogComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<AddUserDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  
+  ngOnInit() {
+  }
+
+}
